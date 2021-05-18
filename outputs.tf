c3d4e09c8d8f7d777177eccea3db38d2d8a87ac3
Kalen <kalenarndt@gmail.com>
2021-05-17 18:43:50 -0600

    terraform fmt
    forgot to change my pre-commit

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "overlay_segments" {
-  value = local.overlay_segment_objs
+  value       = local.overlay_segment_objs
   description = "Overlay Segment output object to use with other modules."
 }
